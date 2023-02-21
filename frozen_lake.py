eee7f63cd27e48f39bafe635074b2e6e01043726
EdwinMeriaux <Edwin_Meriaux@student.uml.edu>
2023-02-21 12:08:21 -0500

    settup with frozen repeat

--- frozen_lake.py
+++ frozen_lake.py
@@ -1,1 +1,24 @@
+import gym
+import numpy as np
 
+env = gym.make('FrozenLake-v1', desc=["SFFF", "FHFH", "FFFH", "HFFG"], map_name="4x4", is_slippery=False)
+state = env.reset()
+env.render()
+action = 0
+obs, reward, terminated, truncated, info = env.step(action)
+print(env.step(action))
+action = 2
+obs, reward, terminated, truncated, info = env.step(action)
+print(env.step(action))
+action = 2
+obs, reward, terminated, truncated, info = env.step(action)
+print(env.step(action))
+
+
+action = 1
+obs, reward, terminated, truncated, info = env.step(action)
+print(env.step(action))
+action = 1
+obs, reward, terminated, truncated, info = env.step(action)
+print(env.step(action))
+print(env.action_space)
